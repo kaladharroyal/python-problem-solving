890e8a1580895112d2bd9be727135ca84af8a35e
kaladharroyal <kaladharroyal@gmail.com>
2026-01-17 09:00:23 +0530

    updated

--- demo-code/hello.py
+++ demo-code/hello.py
@@ -1,5 +1,18 @@
-for i in range(1, 10):
-    print(" ")
-    for j in range(1, 10):
-        if i == j or i >j:
-            print("*")
+n = 5
+
+for i in range(n):
+    # Print spaces for formatting
+    print(" " * (n - i - 1), end="")
+    
+    # Print Pascal's triangle numbers
+    for j in range(i + 1):
+        if j == 0 or j == i:
+            print(1, end=" ")
+        else:
+            # Calculate binomial coefficient
+            val = 1
+            for k in range(1, min(j, i - j) + 1):
+                val = val * (i - k + 1) // k
+            print(val, end=" ")
+    
+    print()  # New line after each row
